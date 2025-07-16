663aa84a56ae05d139cf5d8d354d93553c1a0f2f
Asm Sayem <Asm Sayem@DESKTOP-5C486UG>
2025-07-16 16:51:18 +0600

    feat: add product and variant seed values

--- ProductOrderManagement/Data/AppDbContext.cs
+++ ProductOrderManagement/Data/AppDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using ProductOrderManagement.Enums;
 using ProductOrderManagement.Models;
 
 namespace ProductOrderManagement.Data;
@@ -33,7 +34,22 @@ public class AppDbContext : IdentityDbContext<ApplicationUser>
             .HasOne(oi => oi.Variant)
             .WithMany()
             .HasForeignKey(oi => oi.VariantId)
-            .OnDelete(DeleteBehavior.Restrict); 
+            .OnDelete(DeleteBehavior.Restrict);
+
+        // Seed Products
+        modelBuilder.Entity<Product>().HasData(
+            new Product { Id = 1, Name = "Classic Mug", Brand = "MugBrand", Type = ProductType.Mug },
+            new Product { Id = 2, Name = "Eco Jug", Brand = "JugPro", Type = ProductType.Jug },
+            new Product { Id = 3, Name = "Coffee Cup", Brand = "CupX", Type = ProductType.Cup }
+        );
+
+        // Seed Variants
+        modelBuilder.Entity<Variant>().HasData(
+            new Variant { Id = 1, ProductId = 1, Color = "White", Specification = "Ceramic", Size = Size.Medium },
+            new Variant { Id = 2, ProductId = 1, Color = "Black", Specification = "Steel", Size = Size.Large },
+            new Variant { Id = 3, ProductId = 2, Color = "Blue", Specification = "Plastic", Size = Size.Large },
+            new Variant { Id = 4, ProductId = 3, Color = "Red", Specification = "Glass", Size = Size.Small }
+        );
     }
 
 }
---
+++ ProductOrderManagement/Migrations/20250716104843_add-seed-data.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace ProductOrderManagement.Migrations
+{
+    /// <inheritdoc />
+    public partial class addseeddata : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Brand", "Name", "Type" },
+                values: new object[,]
+                {
+                    { 1, "MugBrand", "Classic Mug", 0 },
+                    { 2, "JugPro", "Eco Jug", 1 },
+                    { 3, "CupX", "Coffee Cup", 2 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Variants",
+                columns: new[] { "Id", "Color", "ProductId", "Size", "Specification" },
+                values: new object[,]
+                {
+                    { 1, "White", 1, 1, "Ceramic" },
+                    { 2, "Black", 1, 2, "Steel" },
+                    { 3, "Blue", 2, 2, "Plastic" },
+                    { 4, "Red", 3, 0, "Glass" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Variants",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Variants",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Variants",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Variants",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
